a2a452fb94473bea38a8cabe7c5bf618d7d30af5
Muhammet Mahmut Gümüş <56482835+mmgumus@users.noreply.github.com>
2023-03-03 13:57:31 +0300

    b
    b

---
+++ Assets/Scripts/Building/CustomizeWalls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[System.Serializable]
+public class CustomizeWalls : MonoBehaviour
+{
+    private Transform[] childs;
+    [HideInInspector]
+    public Color Ground = Color.white;
+    [HideInInspector]
+    public Color Walls = Color.white;
+    [HideInInspector]
+    public Color BorderWalls = Color.white;
+    [HideInInspector]
+    public Color DoorFrame = Color.white;
+    private void LateUpdate()
+    {
+        childs = gameObject.GetComponentsInChildren<Transform>();
+    }
+    public void SetWallColors()
+    {
+        foreach (Transform child in childs)
+        {
+            if (child.gameObject.tag == "Ground")
+            {
+                child.gameObject.GetComponent<SpriteRenderer>().color = Ground;
+            }
+            if (child.gameObject.tag == "Walls")
+            {
+                child.gameObject.GetComponent<SpriteRenderer>().color = Walls;
+            }
+            if (child.gameObject.tag == "BorderWalls")
+            {
+                child.gameObject.GetComponent<SpriteRenderer>().color = BorderWalls;
+            }
+            if (child.gameObject.tag == "DoorFrame")
+            {
+                child.gameObject.GetComponent<SpriteRenderer>().color = DoorFrame;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Building/GroundProperties.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GroundProperties : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.1f,10)]
+    private float scaleX = 1;
+
+    [SerializeField]
+    [Range(0.1f, 10)]
+    private float scaleY = 1;
+    private void LateUpdate()
+    {
+        transform.localScale = new Vector3(scaleX, scaleY);
+    }
+}
